a3bd196cae23db5a93bf4156e5e35a0a20eb1cec
VictoriaWoo <Victoria.woo23@ycdsbk12.ca>
2022-02-18 20:51:22 -0500

    Background, sizing, rainbow drawing

--- Sketch.java
+++ Sketch.java
@@ -7,30 +7,52 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	// size call
+    size(600, 400);
   }
 
   /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * initial set up (background)
    */
   public void setup() {
-    background(210, 255, 173);
+    background(196, 212, 201);
   }
 
   /**
-   * Called repeatedly, anything drawn to the screen goes here
+   * Drawn to the screen
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Ellipses (Rainbow)
+    fill(239, 115, 113);
+    ellipse(300, 350, 400, 400);
+    fill(248, 181, 129);
+    ellipse(300, 375, 360, 400);
+    fill(253, 230, 151);
+    ellipse(300, 400, 320, 400);
+    fill(151, 186, 129);
+    ellipse(300, 425, 280, 400);
+    fill(132, 146, 184);
+    ellipse(300, 450, 240, 400);
+    fill(168, 148, 186, 255);
+    ellipse(300, 475, 200, 400);
+    fill(196, 212, 201);
+    ellipse(300, 500, 160, 400);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  	// Rectangle (Grass)
+    fill(60, 171, 86);
+    rect(0, 350, 600, 50);
+
+    // Circle (Sun)
+    fill(255, 245, 130);
+    circle(600, 0, 150);
+
+    // Lines (Sun Rays)
+    line(500, 20, 460, 40);
+    line(525, 50, 490, 75);
+    line(550, 75, 525, 100);
+    line(580, 90, 570, 120);
+
   }
   
-  // define other methods down here.
 }
