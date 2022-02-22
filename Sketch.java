b7eafcc27ec9082f47628ae0b93245bb9bee62b7
VictoriaWoo <Victoria.woo23@ycdsbk12.ca>
2022-02-22 13:59:28 -0500

    changes with processing variables

--- Sketch.java
+++ Sketch.java
@@ -7,8 +7,8 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// size call
-    size(600, 400);
+	// size call (ratio is 1.5:1)
+    size(750, 500);
   }
 
   /** 
@@ -23,39 +23,37 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-    // Ellipses (Rainbow)
+    // Rainbow
     fill(239, 115, 113);
-    ellipse(300, 350, 400, 400);
+    ellipse(width/2, height * 7/8, width * 2/3, height);
     fill(248, 181, 129);
-    ellipse(300, 375, 360, 400);
+    ellipse(width/2, height * 15/16, width * 3/5, height);
     fill(253, 230, 151);
-    ellipse(300, 400, 320, 400);
+    ellipse(width/2, height, width * 8/15, height);
     fill(151, 186, 129);
-    ellipse(300, 425, 280, 400);
+    ellipse(width/2, height * 17/16, width * 7/15, height);
     fill(132, 146, 184);
-    ellipse(300, 450, 240, 400);
+    ellipse(width/2, height * 9/8, width * 2/5, height);
     fill(168, 148, 186, 255);
-    ellipse(300, 475, 200, 400);
+    ellipse(width/2, height * 19/16, width * 1/3, height);
     fill(196, 212, 201);
-    ellipse(300, 500, 160, 400);
+    ellipse(width/2, height * 5/4, width * 4/15, height);
 
-  	// Rectangle (Grass)
+  	// Grass
     fill(60, 171, 86);
-    rect(0, 350, 600, 50);
+    rect(0, height * 7/8, width, height * 1/8);
 
-    // Circle (Sun)
+    // Sun
     fill(255, 245, 130);
-    circle(600, 0, 150);
+    circle(width, 0, width/4);
+    line(width * 5/6, height * 1/20, width * 23/30, height * 1/10);
+    line(width * 7/8, height * 1/8, width * 49/60, height * 3/16);
+    line(width * 11/12, height * 3/16, width * 7/8, height * 1/4);
+    line(width * 29/30, height * 9/40, width * 19/20, height * 3/10);
 
-    // Lines (Sun Rays)
-    line(500, 20, 460, 40);
-    line(525, 50, 490, 75);
-    line(550, 75, 525, 100);
-    line(580, 90, 570, 120);
-
-    // Quad (path)
+    // Path
     fill(155, 118, 83);
-    quad(290, 350, 310, 350, 360, 400, 240, 400);
+    quad(width * 29/60, height * 7/8, width * 31/60, height * 7/8, width * 3/5, height, width * 2/5, height);
 
   }
   
